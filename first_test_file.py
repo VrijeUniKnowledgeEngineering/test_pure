84accf8b5b619912c2195a2c32d387226aeff96b
Finn <finn.potason@gmail.com>
2018-02-16 10:32:04 +0100

    working

--- first_test_file.py
+++ first_test_file.py
@@ -1,13 +1,23 @@
 import requests
 import json
 
-from pprint import pprint
+data = {"data" : "24.3"}
+data_json = json.dumps(data)
 
-def main():
-    response = requests.get("https://research.vu.nl/ws/api/59/persons?q=harmelen&apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd")
-    pure = response.json()
-    # pprint(pure)
-    pprint(pure)
+# Accept: application/json
 
-if __name__ == "__main__":
-    main()
+headers = {'Accept': 'application/json'}
+
+resp = requests.get('https://research.vu.nl/ws/api/59/persons?q=harmelen&apiKey=1aecc9b3-0b58-4e00-b757-c1a8026cbbfd',  headers=headers)
+if resp.status_code != 200:
+    # This means something went wrong.
+    print('life is hard')
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+print (resp)
+print (resp.status_code)
+print (resp.json())
+
+# for todo_item in resp.json():
+#
+#     print('{} {}'.format(todo_item['id'], todo_item['summary']))
